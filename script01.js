fce8146a4bc33a5dc3b2d857fd51b0056bf60829
ddr669 <dudu.herique.x@gmail.com>
2025-06-27 06:08:00 -0300

    finally

--- script01.js
+++ script01.js
@@ -1,7 +1,7 @@
 
 class NumeroMagico {
     constructor () {
-        this._tentativas = 5;
+        this._tentativas = 10;
         this._RESPOSTA = Number(Math.random().toFixed(2) * 100);
     }
     get GET_TENTATIVAS(){
@@ -22,22 +22,28 @@ function okayPopup(){
 
 function verificaNumero() {
     let tentativa = parseInt(document.getElementById("chute").value);
-    
     console.log(numero.GET_TENTATIVAS);
     if (isNaN(tentativa)) {
         alert("Tente um numero");
         return 0;
     }
+    if (tentativa > 100 || tentativa < -1) {
+        alert("Tente um numero de 0 a 100");
+        return 0;
+
+    }
     console.log(tentativa);
     if (tentativa == numero.GET_RESPOSTA) {
-        document.getElementById("parabens").textContent = `Parabens você acertou! com o total de ${numero.GET_TENTATIVAS - 5} tentativas.`;
+        document.getElementById("parabens").textContent = `Parabens você acertou! com o total de ${10-numero.GET_TENTATIVAS} tentativas.`;
         return 0
-    }else if (numero.GET_TENTATIVAS <= 0){
-        document.getElementById("parabens").textContent = "Você Perdeu :/ ";
+    }else if (numero.GET_TENTATIVAS < 1){
+        document.getElementById("parabens").textContent = "Você Perdeu :/";
+        document.getElementById("resposta").textContent = `A resposta era ${numero.GET_RESPOSTA}`;
     }
     else {
         document.getElementById("chute").value = "";
         numero._tentativas = numero._tentativas - 1;
+        document.getElementById("dica").textContent =  tentativa < numero.GET_RESPOSTA ? "O numero é maior":"O numero é menor";
         document.getElementById("tentativas").textContent = `tentativas restantes: ${numero.GET_TENTATIVAS}`;
     }
 }
